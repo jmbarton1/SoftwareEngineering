61769f2dde3676c6779594139081bb21e1d41683
jmbarton1 <jmbarton1@plymouth.edu>
2014-09-18 16:39:27 -0400

    UploadedNewFiles
    Just need to figure that MouseClick out...

--- PlayChomp.java
+++ PlayChomp.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.io.*;
 import java.util.*;
 import java.awt.*;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
 
 public class PlayChomp{
 
@@ -54,33 +56,83 @@ public class PlayChomp{
 		//update array
 		//call to update the board
 
+
 	}
 
 	public static void main(String[] args){
 		Chomp game = new Chomp();
 		PlayChomp PlayGame = new PlayChomp(game);
 		PlayGame.playGame();
+
 	}
 }
 class DisplayGame extends JFrame{
 	Chomp game;
+	JButton buttons[][] = new JButton[3][5];
+
+	public void killJButtons(int row, int col){
+		 buttons[row][col].setVisible(false);
+	}
+
 	public DisplayGame(Chomp game){
 		this.game=game;
 	}
 	public void displayGame(){
+
 		JPanel p1 = new JPanel();
-		p1.setLayout(new GridLayout(game.getRow(),game.getCol(),5,5));
+
+		p1.setLayout(new GridLayout(3,0,5,5));
 		p1.setOpaque(true);
-		for(int i = 0; i < 15; i++){
-			if(i == 0){
-				p1.add(new JButton("p"));
+		for(int i = 0; i < 3; i++){
+			for(int j = 0; j<5; j++){
+				if(i == 0&&j==0){//poison cookie
+					buttons[i][j] = new JButton(" "+i+", "+j);
+					buttons[i][j].addMouseListener(new MouseClick());
+					buttons[i][j].putClientProperty("row", i);
+					buttons[i][j].putClientProperty("column", j);
+					buttons[i][j].putClientProperty("poison", true);
+					p1.add(buttons[i][j]);
+
+				}
+				else{
+					buttons[i][j] = new JButton(" "+i+", "+j);
+					buttons[i][j].addMouseListener(new MouseClick());
+					buttons[i][j].putClientProperty("row", i);
+					buttons[i][j].putClientProperty("column", j);
+					p1.add(buttons[i][j]);
+
+
+				}
 			}
-			p1.add(new JButton("c"));
+
 		}
 		this.setContentPane(p1);
 		this.pack();
 		this.repaint();
-		//add(p1);
 
 	}
+}
+
+
+class MouseClick implements MouseListener {
+
+	public void mouseClicked(MouseEvent e){
+		JButton test = (JButton)e.getSource();
+		System.out.println(e.getSource());
+		int rowTest = (int)test.getClientProperty("row");
+		int columnTest = (int)test.getClientProperty("column");
+		//buttons[rowTest][columnTest].setVisible(false);
+		//.killJButtons(rowTest, columnTest);
+	}
+
+		public void mousePressed(MouseEvent e){
+	}
+		public void mouseReleased(MouseEvent e){
+	}
+		public void mouseEntered(MouseEvent e){
+	}
+		public void mouseExited(MouseEvent e){
+	}
+
+
 }
